0502c5803657b400de3cb8747acf331ae3b54fc3
Haoyan <657290301@qq.com>
2022-03-12 10:55:13 +0800

    10点55分 3.12

--- pa1/main.cpp
+++ pa1/main.cpp
@@ -3,14 +3,16 @@
 #include <eigen3/Eigen/Eigen>
 #include <iostream>
 #include <opencv2/opencv.hpp>
-
+using namespace std;
+using namespace cv;
+using namespace Eigen;
 constexpr double MY_PI = 3.1415926;
 
-Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
+Matrix4f get_view_matrix(Vector3f eye_pos)
 {
-    Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
+    Matrix4f view = Matrix4f::Identity();
 
-    Eigen::Matrix4f translate;
+    Matrix4f translate;
     translate << 1, 0, 0, -eye_pos[0], 0, 1, 0, -eye_pos[1], 0, 0, 1,
         -eye_pos[2], 0, 0, 0, 1;
 
@@ -19,28 +21,46 @@ Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
     return view;
 }
 
-Eigen::Matrix4f get_model_matrix(float rotation_angle)
+Matrix4f get_model_matrix(float rotation_angle)
 {
-    Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
+    Matrix4f model = Matrix4f::Identity();
 
     // TODO: Implement this function
     // Create the model matrix for rotating the triangle around the Z axis.
     // Then return it.
-
+    float delta=rotation_angle;
+    float fa=acos(-1)*delta/180;
+    model<<cos(fa),-sin(fa),0,0,sin(fa),cos(fa),0,0,0,0,1,0,0,0,0,1;
     return model;
 }
 
-Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
+Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
                                       float zNear, float zFar)
 {
     // Students will implement this function
 
-    Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
-
+    Matrix4f projection = Matrix4f::Identity();
+    Matrix4f p,o,n;
     // TODO: Implement this function
     // Create the projection matrix for the given parameters.
     // Then return it.
-
+    p<< zNear,0,0,0, 
+        0,zNear,0,0, 
+        0,0,zNear+zFar,-zNear*zFar, 
+        0,0,1,0;
+    float t=tan(eye_fov/2/180*acos(-1))*zNear;
+    float b=-t;
+    float r=t*aspect_ratio;
+    float l=-r;
+    o<< 2/(r-l),0,0,0, 
+        0,2/(t-b),0,0, 
+        0,0,2/(zNear-zFar),0, 
+        0,0,0,1;
+    n<< 1,0,0,-(r+l)/2, 
+        0,1,0,-(t+b)/2, 
+        0,0,1,-(zNear+zFar)/2, 
+        0,0,0,1;
+    projection=n*o*p;
     return projection;
 }
 
@@ -48,13 +68,13 @@ int main(int argc, const char** argv)
 {
     float angle = 0;
     bool command_line = false;
-    std::string filename = "output.png";
+    string filename = "output.png";
 
     if (argc >= 3) {
         command_line = true;
-        angle = std::stof(argv[2]); // -r by default
+        angle = stof(argv[2]); // -r by default
         if (argc == 4) {
-            filename = std::string(argv[3]);
+            filename = string(argv[3]);
         }
         else
             return 0;
@@ -62,11 +82,11 @@ int main(int argc, const char** argv)
 
     rst::rasterizer r(700, 700);
 
-    Eigen::Vector3f eye_pos = {0, 0, 5};
+    Vector3f eye_pos = {0, 0, 5};
 
-    std::vector<Eigen::Vector3f> pos{{2, 0, -2}, {0, 2, -2}, {-2, 0, -2}};
+    vector<Vector3f> pos{{2, 0, -2}, {0, 2, -2}, {-2, 0, -2}};
 
-    std::vector<Eigen::Vector3i> ind{{0, 1, 2}};
+    vector<Vector3i> ind{{0, 1, 2}};
 
     auto pos_id = r.load_positions(pos);
     auto ind_id = r.load_indices(ind);
@@ -82,10 +102,10 @@ int main(int argc, const char** argv)
         r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
 
         r.draw(pos_id, ind_id, rst::Primitive::Triangle);
-        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+        Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
         image.convertTo(image, CV_8UC3, 1.0f);
 
-        cv::imwrite(filename, image);
+        imwrite(filename, image);
 
         return 0;
     }
@@ -99,12 +119,12 @@ int main(int argc, const char** argv)
 
         r.draw(pos_id, ind_id, rst::Primitive::Triangle);
 
-        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+        Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
         image.convertTo(image, CV_8UC3, 1.0f);
-        cv::imshow("image", image);
-        key = cv::waitKey(10);
+        imshow("image", image);
+        key = waitKey(10);
 
-        std::cout << "frame count: " << frame_count++ << '\n';
+        cout << "frame count: " << frame_count++ << '\n';
 
         if (key == 'a') {
             angle += 10;
